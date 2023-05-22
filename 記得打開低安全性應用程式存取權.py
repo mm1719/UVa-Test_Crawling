beb59874cd1e015e39eb2778e23352fc2668e7f9
mm1719 <91235741+mm1719@users.noreply.github.com>
2023-05-22 22:58:54 +0800

    Add files via upload

---
+++ 記得打開低安全性應用程式存取權.py
@@ -0,0 +1,21 @@
+import smtplib
+import sys
+from smtplib import SMTP
+
+From = 'sending.account@gmail.com'
+Password = 'yourpassword'
+To = 'receiving.account@gmail.com'
+
+line = sys.stdin.read()
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+smtpObj = smtplib.SMTP(smtp_server, smtp_port)
+smtpObj.starttls()
+smtpObj.login(From,Password)
+to = [To]
+msg = line
+
+smtpObj.sendmail(From, to, msg)
+smtpObj.quit()
